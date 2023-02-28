6393ed711dda24cf4ccc6d097310e484fd7e38c1
= <=>
2023-02-28 20:06:50 +0100

    Primeros pasos

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ChatListItem from './components/atoms/chatListItem';
+
+const chat = {
+  id: '1',
+  user: {
+    image:'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg',
+    name: 'Lukas',
+  },
+  lastMessage: {
+    text: 'Oke',
+    createdAt: '07:30',
+  },
+};
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+     
+      <ChatListItem chat={chat}/>
+      
       <StatusBar style="auto" />
+
     </View>
   );
 }
---
+++ components/atoms/chatListItem.js
@@ -0,0 +1,51 @@
+import {Text, View, Image, StyleSheet} from 'react-native';
+
+
+const ChatListItem = ({chat}) => {
+   return(
+      <View id={chat.id} style={styles.container}>
+         <Image style={styles.image} source={{uri: chat.user.image ?? 'https://static.fundacion-affinity.org/cdn/farfuture/PVbbIC-0M9y4fPbbCsdvAD8bcjjtbFc0NSP3lRwlWcE/mtime:1643275542/sites/default/files/los-10-sonidos-principales-del-perro.jpg'}} />
+         <View style={styles.content}>
+            <View style={styles.messageInfo}>
+               <Text numberOfLines={1} style={styles.name}>{chat.user.name}</Text>
+               <Text style={styles.date}>{chat.lastMessage.createdAt}</Text>
+            </View>
+            <Text numberOfLines={2} style={styles.date}>{chat.lastMessage.text}</Text>
+         </View>
+      </View>
+   )
+}
+
+const styles = StyleSheet.create({
+   container:{
+      flexDirection: 'row',
+      marginHorizontal: 10,
+      height: 70,
+      
+   },
+   image:{
+      width:60, 
+      height:60,
+      borderRadius: 30,
+      marginRight: 10,
+   },
+   content:{
+      flex: 1,
+   },
+   messageInfo:{
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      marginBottom: 2,
+   },
+   name:{
+      fontWeight: 'bold',
+   },
+   date:{
+      fontWeight: '300',
+      color: 'gray',
+   },
+})
+
+
+
+export default ChatListItem;
