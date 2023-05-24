d53350da277063b562325aa66d65ae58248d3afb
= <=>
2023-05-24 20:04:20 +0200

    feat: crear los chats se muestran en tiempo real y nuevos mensajes se muestran en tiempo real

--- src/screens/ListOfChatsScreen.js
+++ src/screens/ListOfChatsScreen.js
@@ -3,22 +3,59 @@ import React, { useEffect, useState } from 'react'
 import ListarComponentes from '../components/molecules/ListarComponenets'
 import { API, graphqlOperation, Auth } from 'aws-amplify'
 import { listChatRooms } from '../CustomQueries/queries'
+import { onCreateChatRoom, onUpdateChatRoom } from '../graphql/subscriptions'
 
 const ListOfChatsScreen = () => {
   const [chatRooms, setChatRooms] = useState([])
+  const [loading, setLoding] = useState(false);
   const fetchChatRooms = async () => {
+    setLoding(true);
     const userAutenticated = await Auth.currentAuthenticatedUser()
     const response = await API.graphql(
       graphqlOperation(listChatRooms, { id: userAutenticated.attributes.sub })
     )
     const rooms = response?.data?.getUser?.ChatRoomsUsers?.items || []
 
+    const sortedRooms = orderRooms(rooms)
+    setChatRooms(sortedRooms)
+    setLoding(false);
+  }
+
+  const orderRooms = (rooms) =>{
+    
     const sortedRooms = rooms.sort(
       (r1, r2) =>
         new Date(r2.chatRoom?.updatedAt) - new Date(r1.chatRoom?.updatedAt)
     )
-    setChatRooms(sortedRooms)
+    
+    return sortedRooms;
   }
+
+  useEffect(() => {
+    const subscription = API.graphql(
+      graphqlOperation(onUpdateChatRoom)
+    ).subscribe({
+      next: () => {
+        fetchChatRooms()
+      },
+      error: (err) => console.warn(err),
+    })
+
+    return () => subscription.unsubscribe()
+  }, [])
+
+  useEffect(() => {
+    const subscription = API.graphql(
+      graphqlOperation(onCreateChatRoom)
+    ).subscribe({
+      next: () => {
+        fetchChatRooms()
+      },
+      error: (err) => console.warn(err),
+    })
+
+    return () => subscription.unsubscribe()
+  }, [])
 
   useEffect(() => {
     fetchChatRooms()
@@ -26,7 +63,7 @@ const ListOfChatsScreen = () => {
 
   return (
     <View style={styles.container}>
-      <ListarComponentes listaMensajes={chatRooms} tipo="chat" />
+      <ListarComponentes listaMensajes={chatRooms} onRefresh={fetchChatRooms} refreshing={loading} tipo="chat" />
     </View>
   )
 }
